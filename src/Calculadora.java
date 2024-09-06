41708fbf15b038ba5364df4bd2bf9ca7c2ce945e
Ricardo García <hazelgrmtz8@gmail.com>
2024-09-05 23:10:06 -0600

    Proyecto inicial                    Agregación y modificación de los botones >>Se agregó las clases de las operaciones

--- src/Calculadora.java
+++ src/Calculadora.java
@@ -1,28 +1,30 @@
 public class Calculadora {
 
-    public String sumar(String a, String b){
+    public String sumar(String a, String b) {
         String res = "";
-        res = (double)(Integer.parseInt(a) + Integer.parseInt(b)) + "";
+        res = (double) (Integer.parseInt(a) + Integer.parseInt(b)) + "";
         return res;
 
-}
-    public String resta(String a, String b){
+    }
+
+    public String resta(String a, String b) {
         String res = "";
         res = (Integer.parseInt(a) - Integer.parseInt(b)) + "";
         return res;
 
     }
 
-    public String multiplicar(String a, String b){
+    public String multiplicar(String a, String b) {
         String res = "";
         res = (Integer.parseInt(a) * Integer.parseInt(b)) + "";
         return res;
 
     }
 
-    public String dividir(String a, String b){
+    public String dividir(String a, String b) {
         String res = "";
         res = (Integer.parseInt(a) / Integer.parseInt(b)) + "";
         return res;
 
     }
+}
